dc153afff47796462a4722be9d5b5dff7766a6a4
Duy Tran <14388@student.vgu.edu.vn>
2021-05-18 14:45:49 +0700

    update practice test and http

---
+++ Practice-test/ex1-RMI/MediaManagement.java
@@ -0,0 +1,16 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.attribute.standard.Media;
+
+public interface MediaManagement extends Remote {
+    List<Media> getBooks() throws RemoteException;
+    List<Media> getNews() throws RemoteException;
+    boolean login(String username, String password) throws RemoteException;
+    void play(int pos1, int pos2, int character) throws RemoteException;
+    String getWinner() throws RemoteException;
+    int[][] getTic() throws RemoteException;
+    int getSlot() throws RemoteException;
+}
